96c69b0eef3ca5ec8f0f6ad0d5a869d6dbfc3f6c
Misa <mamadorgonzalez@horizon.csueastbay.edu>
2024-02-25 23:03:51 -0800

    Current Program

--- src/Address/AddressBook.java
+++ src/Address/AddressBook.java
@@ -1,5 +1,6 @@
 package Address;
 import java.util.LinkedList;
+import java.util.Collections;
 import Address.data.AddressEntry;
 
 class AddressBook{
@@ -8,7 +9,7 @@ class AddressBook{
     public AddressBook(){
         this.addressEntryList = new LinkedList<>();
     }
-    public void list(){  //list which iterates through address entrylist and for each item call toString &prints it out
+    public void list(){  //list which iterates through address entry list and for each item call toString & prints it out
         for (AddressEntry addressEntry : addressEntryList){
             System.out.println(addressEntry.toString());
         }
@@ -16,5 +17,19 @@ class AddressBook{
     public void add(AddressEntry addressEntry){
         addressEntryList.add(addressEntry);
     }
-
+    public void remove(AddressEntry addressEntry){
+        addressEntryList.remove(addressEntry);
+    }
+    public AddressEntry find(String lastName){
+        for(AddressEntry entry : addressEntryList){
+            if(entry.getLastName().equalsIgnoreCase(lastName)){
+                return entry;
+            }
+        }
+        return null;
+    }
+    public void listAlphabeticOrder(){
+        Collections.sort(addressEntryList, (entry1, entry2) -> entry1.getLastName().compareTo(entry2.getLastName()));
+        list();
+    }
 }
